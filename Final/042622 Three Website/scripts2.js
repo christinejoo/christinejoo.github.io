fb7816879a95d3d59a9c5bdf7320efe7cf571c9a
jooc749 <jooc749@newschool.edu>
2022-04-27 12:08:29 -0400

    a-gen-web final website

---
+++ Final/042622 Three Website/scripts2.js
@@ -0,0 +1,67 @@
+let angle = 0
+let art
+var shapeCount = 0
+
+
+function setup() {
+    art = createGraphics(400, 400)
+      noStroke();
+    art.fill(255,255,255);
+    art.rect(0,0,420,420);
+  }
+  
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    art.beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      art.vertex(sx, sy);
+    }
+    art.endShape(CLOSE);
+  }
+  
+  function draw() {
+    if (shapeCount <= 100){
+      art.fill(random(225), random(225), random(225))
+      let x = random(400);
+      let y = random(400);
+  
+      
+     
+        // let shape = random(['triangle', 'square', 'ellipse']);
+        // if(shape === 'triangle') {
+        //   art.triangle(x, y, x+random(80), y+random(80),x+random(80), y+random(80));
+        // }else if(shape === 'square') {
+        //   art.square(x,y,30)
+        // }else {
+        //   art.ellipse(x, y, 30,30);
+        // }
+        let npoints = random ([3,4,5,6]);
+        polygon(x, y, 30, npoints);
+  
+      
+    }
+    // background(0,0,0,0);
+  //   push()
+  // //   translate(200, 200)
+  //   // let x = random(0, width)
+  
+  //   // let y = random(0, height)
+  
+    
+  //   // c += 0.2
+  //   // a += 0.8
+  //   pop()
+  
+    push()
+    texture(art)
+  
+    angle += 0.000;
+    pop()
+  
+    shapeCount++
+  
+    
+  }
